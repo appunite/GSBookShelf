db9a2fbd609af70072f8dc6f7eb85a1734ed9599
Natalia Osiecka <natalia.osa@appunite.com>
2012-11-17 00:32:56 +0100

    Added static library

---
+++ BookShelfLib/BookShelfLib.h
@@ -0,0 +1,13 @@
+//
+//  BookShelfLib.h
+//  BookShelfLib
+//
+//  Created by Natalia Osiecka on 16.11.2012.
+//  Copyright (c) 2012 FoOTOo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BookShelfLib : NSObject
+
+@end
---
+++ BookShelfLib/BookShelfLib.m
@@ -0,0 +1,13 @@
+//
+//  BookShelfLib.m
+//  BookShelfLib
+//
+//  Created by Natalia Osiecka on 16.11.2012.
+//  Copyright (c) 2012 FoOTOo. All rights reserved.
+//
+
+#import "BookShelfLib.h"
+
+@implementation BookShelfLib
+
+@end
